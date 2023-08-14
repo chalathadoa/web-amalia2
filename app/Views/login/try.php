252981459e547d128d510296dfcc47e14460b3d4
chalathadoa <udkhul.uul28@gmail.com>
2023-08-14 13:56:21 +0700

    try2 (viewlogin done)

---
+++ app/Views/login/try.php
@@ -0,0 +1,168 @@
+<div>
+    <link href="./frame2.css" rel="stylesheet" />
+    <div class="frame2-container">
+        <div class="frame2-frame2">
+            <div class="frame2-sidebar">
+                <span class="frame2-text">
+                    <span>
+                        <span>AMALIA KHAIRUN</span>
+                        <br />
+                        <span>NISA</span>
+                    </span>
+                </span>
+                <span class="frame2-text05"><span>pengurus</span></span>
+                <img src alt="Ellipse142088" class="frame2-ellipse14" />
+                <div class="frame2-group3">
+                    <img src alt="Rectangle34044" class="frame2-rectangle3" />
+                    <div class="frame2-logout">
+                        <img src="/uilexit4044-agu.svg" alt="uilexit4044" class="frame2-uilexit" />
+                        <span class="frame2-text07"><span>Log out</span></span>
+                    </div>
+                    <div class="frame2-dashboard">
+                        <span class="frame2-text09"><span>Dashboard</span></span>
+                        <img src="/mdihouse4044-y8hp.svg" alt="mdihouse4044" class="frame2-mdihouse" />
+                    </div>
+                    <div class="frame2-manageuser">
+                        <span class="frame2-text11"><span>Manage User</span></span>
+                        <img src="/oouiuseravataroutline4044-3a0t.svg" alt="oouiuseravataroutline4044"
+                            class="frame2-oouiuseravataroutline" />
+                    </div>
+                    <div class="frame2-asatidz">
+                        <span class="frame2-text13"><span>Jama’ah</span></span>
+                        <img src="/materialsymbolscalendarmonth4044-fn19.svg" alt="materialsymbolscalendarmonth4044"
+                            class="frame2-materialsymbolscalendarmonth" />
+                    </div>
+                    <div class="frame2-santriwati">
+                        <span class="frame2-text15"><span>Laporan Santri</span></span>
+                        <img src="/materialsymbolscalendarmonth4044-r13a.svg" alt="materialsymbolscalendarmonth4044"
+                            class="frame2-materialsymbolscalendarmonth1" />
+                    </div>
+                    <div class="frame2-manageevent">
+                        <span class="frame2-text17"><span>Manage event</span></span>
+                        <img src="/pajamasplanning4044-w4sa.svg" alt="pajamasplanning4044"
+                            class="frame2-pajamasplanning" />
+                    </div>
+                    <div class="frame2-santriwati1">
+                        <span class="frame2-text19"><span>Santriwati</span></span>
+                        <img src="/materialsymbolscalendarmonth4044-44gs.svg" alt="materialsymbolscalendarmonth4044"
+                            class="frame2-materialsymbolscalendarmonth2" />
+                    </div>
+                </div>
+            </div>
+            <span class="frame2-text21"><span>AMALIA HOUSE OF MUSLIMAH</span></span>
+            <span class="frame2-text23">
+                <span>
+                    <span>22 APRIL 2023</span>
+                    <br />
+                    <span>10:12</span>
+                </span>
+            </span>
+            <div class="frame2-laporanstok">
+                <span class="frame2-text28"><span>USERS</span></span>
+                <div class="frame2-tabelstokobat">
+                    <div class="frame2-tabelll">
+                        <img src alt="Rectangle532085" class="frame2-rectangle53" />
+                        <img src alt="Rectangle522085" class="frame2-rectangle52" />
+                        <img src alt="Rectangle502085" class="frame2-rectangle50" />
+                        <img src alt="Rectangle512085" class="frame2-rectangle51" />
+                        <img src alt="Rectangle542085" class="frame2-rectangle54" />
+                    </div>
+                    <div class="frame2-tabellid">
+                        <img src alt="Rectangle532085" class="frame2-rectangle531" />
+                        <img src alt="Rectangle522085" class="frame2-rectangle521" />
+                        <img src alt="Rectangle502085" class="frame2-rectangle501" />
+                        <img src alt="Rectangle512086" class="frame2-rectangle511" />
+                        <img src alt="Rectangle542086" class="frame2-rectangle541" />
+                        <span class="frame2-text30"><span>ID User</span></span>
+                    </div>
+                    <div class="frame2-tabellid1">
+                        <img src alt="Rectangle532086" class="frame2-rectangle532" />
+                        <img src alt="Rectangle522086" class="frame2-rectangle522" />
+                        <img src alt="Rectangle502086" class="frame2-rectangle502" />
+                        <img src alt="Rectangle512086" class="frame2-rectangle512" />
+                        <img src alt="Rectangle542086" class="frame2-rectangle542" />
+                        <span class="frame2-text32"><span>Status</span></span>
+                    </div>
+                    <div class="frame2-tabellnama">
+                        <img src alt="Rectangle532087" class="frame2-rectangle533" />
+                        <img src alt="Rectangle522087" class="frame2-rectangle523" />
+                        <img src alt="Rectangle502087" class="frame2-rectangle503" />
+                        <img src alt="Rectangle512087" class="frame2-rectangle513" />
+                        <img src alt="Rectangle542087" class="frame2-rectangle543" />
+                        <span class="frame2-text34"><span>Nama Lengkap</span></span>
+                    </div>
+                    <div class="frame2-tabellnama1">
+                        <img src alt="Rectangle532088" class="frame2-rectangle534" />
+                        <img src alt="Rectangle522088" class="frame2-rectangle524" />
+                        <img src alt="Rectangle502088" class="frame2-rectangle504" />
+                        <img src alt="Rectangle512088" class="frame2-rectangle514" />
+                        <img src alt="Rectangle542088" class="frame2-rectangle544" />
+                        <span class="frame2-text36"><span>Email</span></span>
+                    </div>
+                    <div class="frame2-tabellnama2">
+                        <img src alt="Rectangle532151" class="frame2-rectangle535" />
+                        <img src alt="Rectangle522151" class="frame2-rectangle525" />
+                        <img src alt="Rectangle502151" class="frame2-rectangle505" />
+                        <img src alt="Rectangle512152" class="frame2-rectangle515" />
+                        <img src alt="Rectangle542152" class="frame2-rectangle545" />
+                        <span class="frame2-text38"><span>Prodi</span></span>
+                    </div>
+                    <div class="frame2-tabellupdatedby">
+                        <div class="frame2-group98">
+                            <div class="frame2-editdelete">
+                                <img src alt="pencil12089" class="frame2-pencil1" />
+                                <img src alt="delete112089" class="frame2-delete11" />
+                            </div>
+                        </div>
+                        <div class="frame2-group97">
+                            <div class="frame2-editdelete1">
+                                <img src alt="pencil12081" class="frame2-pencil11" />
+                                <img src alt="delete112081" class="frame2-delete111" />
+                            </div>
+                        </div>
+                        <img src alt="Rectangle502081" class="frame2-rectangle506" />
+                        <div class="frame2-group96">
+                            <div class="frame2-editdelete2">
+                                <img src alt="pencil12081" class="frame2-pencil12" />
+                                <img src alt="delete112081" class="frame2-delete112" />
+                            </div>
+                        </div>
+                        <div class="frame2-group99">
+                            <div class="frame2-editdelete3">
+                                <img src alt="pencil12081" class="frame2-pencil13" />
+                                <img src alt="delete112081" class="frame2-delete113" />
+                            </div>
+                        </div>
+                        <span class="frame2-text40"><span>Operasi</span></span>
+                    </div>
+                    <span class="frame2-text42"><span>No.</span></span>
+                    <div class="frame2-tabellnama3">
+                        <img src alt="Rectangle532081" class="frame2-rectangle536" />
+                        <img src alt="Rectangle522081" class="frame2-rectangle526" />
+                        <img src alt="Rectangle502081" class="frame2-rectangle507" />
+                        <img src alt="Rectangle512081" class="frame2-rectangle516" />
+                        <img src alt="Rectangle542081" class="frame2-rectangle546" />
+                        <span class="frame2-text44"><span>No.HP</span></span>
+                    </div>
+                    <div class="frame2-tabellnama4">
+                        <img src alt="Rectangle532081" class="frame2-rectangle537" />
+                        <img src alt="Rectangle522081" class="frame2-rectangle527" />
+                        <img src alt="Rectangle502081" class="frame2-rectangle508" />
+                        <img src alt="Rectangle512081" class="frame2-rectangle517" />
+                        <img src alt="Rectangle542081" class="frame2-rectangle547" />
+                        <span class="frame2-text46"><span>Password</span></span>
+                    </div>
+                </div>
+                <div class="frame2-addpayment">
+                    <span class="frame2-text48"><span>Add User</span></span>
+                </div>
+                <div class="frame2-search">
+                    <span class="frame2-text50"><span>Cnth: Amalia</span></span>
+                    <img src="/line12081-qggg.svg" alt="Line12081" class="frame2-line1" />
+                    <img src alt="close12081" class="frame2-close1" />
+                    <img src alt="magnifyingglass12081" class="frame2-magnifyingglass1" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
